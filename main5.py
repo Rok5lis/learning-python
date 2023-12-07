f3bd6f59dac9dbe68017f793a1c2d77069ac1624
Rokas <rokas.slankauskas@gmail.com>
2023-12-07 23:20:40 +0200

    pzdc sake 5min

---
+++ main5.py
@@ -0,0 +1,13 @@
+weigh = input('what is your weigh? ')
+qs = input('is it kg or lbs? ')
+if qs == "kg":
+    print(int(weigh)*2.2) 
+elif qs == "lbs":
+    print ("Your weigh :(int(weigh)/2.2)")
+
+    
+
+
+
+
+
